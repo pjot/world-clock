97df2bcc90a95053556bb183142924e7a6045143
Peter Bergström <petb@redpill-linpro.com>
2014-05-13 13:12:30 +0200

    Added script

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 world-clock
 ===========
 
-Small Python script to figure out what time it is in different timezones
+Small Python script to figure out what time it is in different timezones. Just symlink the world-clock.py file into your $PATH.
+
+Usage: world-clock <timezone>
+
+world-clock matches the entered timezone against a list of all timezones and prints the local time in all of them
---
+++ world-clock.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import sys
+import pytz
+import re
+from datetime import datetime
+
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+
+def get_timezones(partial):
+    regex = re.compile(re.escape(partial))
+    for timezone in pytz.all_timezones:
+        if re.search(regex, timezone):
+            yield timezone
+
+
+def run(partial):
+    for timezone in get_timezones(partial):
+        tz = pytz.timezone(timezone)
+        print datetime.now(tz).strftime(DATE_FORMAT), timezone
+
+if len(sys.argv) == 1:
+    print "Usage: world-clock <timezone>"
+    print
+    print "world-clock matches the entered timezone against \
+a list of all timezones and prints the local time \
+in all of them"
+else:
+    run(sys.argv[1])
